d40c815e536520a13d3db005c099ec654b65c965
asdfghjkl <maxlongint1010@gmail.com>
2021-01-11 02:03:54 +0200

    Fix GATE not setup when application is closed

--- app/src/main/java/com/example/gate/listener/SmsListener.java
+++ app/src/main/java/com/example/gate/listener/SmsListener.java
@@ -12,7 +12,6 @@ import android.widget.Toast;
 
 import com.example.gate.Gate;
 import com.example.gate.Individual;
-import com.example.gate.activity.MainActivity;
 import com.example.gate.R;
 
 public class SmsListener extends BroadcastReceiver {
@@ -22,12 +21,13 @@ public class SmsListener extends BroadcastReceiver {
             return;
         }
 
-        if (MainActivity.Gate.SETUP_ERROR != null) {
+        Gate gate = new Gate(context);
+        if (gate.SETUP_ERROR != null) {
             Log.d("SmsListener", "Skipping checking sms: Gate setup failed.");
             return;
         }
 
-        if (!MainActivity.Gate.isAutoCallEnabled()) {
+        if (!gate.isAutoCallEnabled()) {
             Log.d("SmsListener", "Skipping checking sms: Auto Call is not enabled.");
             return;
         }
@@ -43,7 +43,7 @@ public class SmsListener extends BroadcastReceiver {
                 for (int i = 0; i < msgs.length; i++) {
                     msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
                     String msgPhone = msgs[i].getOriginatingAddress();
-                    Individual msgIndividual = Gate.FindGuyByPhone(MainActivity.Gate.GATE_ALLOWED_INDIVIDUALS, msgPhone);
+                    Individual msgIndividual = Gate.FindGuyByPhone(gate.GATE_ALLOWED_INDIVIDUALS, msgPhone);
                     if (msgIndividual == null) {
                         Log.i("SmsListener", "Ignoring message: Sender is not allowed to request to open the gate.");
                         return;
@@ -53,9 +53,9 @@ public class SmsListener extends BroadcastReceiver {
                         Log.i("SmsListener", "Received OPEN message from " + msgIndividual.Name + ".");
                         Intent callIntent = new Intent(Intent.ACTION_CALL);
                         callIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                        callIntent.setData(Uri.parse("tel:" + MainActivity.Gate.GATE_INDIVIDUAL.Phone));
+                        callIntent.setData(Uri.parse("tel:" + gate.GATE_INDIVIDUAL.Phone));
                         context.startActivity(callIntent);
-                        Log.i("SmsListener", String.format("Calling %s...", MainActivity.Gate.GATE_INDIVIDUAL.Name));
+                        Log.i("SmsListener", String.format("Calling %s...", gate.GATE_INDIVIDUAL.Name));
                     } else {
                         Log.i("SmsListener", "Ignoring message: Received non-OPEN message from " + msgIndividual.Name + ".");
                     }
